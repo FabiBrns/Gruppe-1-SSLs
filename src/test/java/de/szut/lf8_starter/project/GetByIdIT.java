5ca1d8a82a868a1a63760aab84ccfb30a9791dd3
Manuel Gilles <117361287+manug243@users.noreply.github.com>
2024-11-06 08:33:59 +0100

    feat: added integration test for getById endpoint

---
+++ src/test/java/de/szut/lf8_starter/project/GetByIdIT.java
@@ -0,0 +1,44 @@
+package de.szut.lf8_starter.project;
+
+import de.szut.lf8_starter.testcontainers.AbstractIntegrationTest;
+import org.junit.jupiter.api.Test;
+import org.springframework.security.test.context.support.WithMockUser;
+
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+public class GetByIdIT extends AbstractIntegrationTest {
+    @Test
+    void authorization() throws Exception {
+        this.mockMvc.perform(get("/project/1")
+                        .with(csrf()))
+                .andExpect(status().isUnauthorized());
+    }
+
+    @Test
+    @WithMockUser(roles = "user")
+    void findById() throws Exception {
+        var stored = projectRepository.save(new ProjectEntity());
+
+        this.mockMvc.perform(get("/project/" + stored.getId())
+                        .param("id", stored.getId().toString())
+                        .with(csrf()))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id").value(stored.getId()));
+    }
+
+    @Test
+    @WithMockUser(roles = "user")
+    void findById_notFound() throws Exception {
+        var stored = new ProjectEntity();
+        stored.setId(999L);
+        projectRepository.save(stored);
+
+        this.mockMvc.perform(get("/project/" + stored.getId())
+                        .param("id", String.valueOf(stored.getId()))
+                        .with(csrf()))
+                .andExpect(status().isNotFound());
+    }
+}
